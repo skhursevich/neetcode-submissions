273fc694c31477f5bdbbdb59cfb5e8c8ac858f22
Sergey Khursevich <47611557+skhursevich@users.noreply.github.com>
2026-07-01 18:14:43 +0200

    Add: string-encode-and-decode - submission-2

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-2.ts
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @returns {string}
+     */
+    encode(strs: string[]): string {
+        return strs.map((str) => `${str.length}/${str}`).join('');
+    }
+
+    /**
+     * @param {string} str
+     * @returns {string[]}
+     */
+    decode(str: string): string[] {
+        const res: string[] = []
+        const data = str.split('');
+
+        for (let i = 0; i < data.length; i++) {
+            if (data[i] !== '/') continue
+
+            const count = data[i - 1];
+            const word = data.slice(i + 1, parseInt(count) + i + 1)
+
+            if (word.length === 0) continue
+
+            res.push(word.join(''))
+        }
+
+        return res;
+    }
+}
