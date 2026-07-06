9ac75a3dcb5122a322ab6f87d7754d7592145004
Sergey Khursevich <47611557+skhursevich@users.noreply.github.com>
2026-07-06 22:33:41 +0200

    Add: two-integer-sum-ii - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-0.ts
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {number[]} numbers
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(numbers: number[], target: number): number[] {
+        let i = 0;
+        let j = numbers.length - 1;
+
+        while (i < j) {
+            const sum = numbers[i] + numbers[j];
+            if (sum === target) {
+                return [i + 1, j + 1];
+            }
+
+            if (sum < target) {
+                i++;
+            } else {
+                j--;
+            }
+        }
+
+        return [];
+    }
+}
